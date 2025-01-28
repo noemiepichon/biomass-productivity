761c0b1a90c4c29ad6fc21b27368726d56d604fe
Noémie Pichon <noemie.pichon@wsl.ch>
2025-01-28 22:03:39 +0100

    one main file

--- biomass-productivity-script.R
+++ biomass-productivity-script.R
@@ -11,7 +11,9 @@ library(nlme)
 rm(list = ls())
 #
 
-
+biomass_productivity_main_ds <- read.table("biomass_productivity_main_ds.txt", header = T)
+regrowth_main <- read.table("regrowth_main.txt", header = T)
+#
 
 #### Statistics main models ####
 
@@ -20,8 +22,7 @@ rm(list = ls())
 # live and dead biomass
 # without negative values
 
-productivity_main <- read.table("productivity_main.txt", header = T)
-
+productivity_main <- biomass_productivity_main_ds[!is.na(biomass_productivity_main_ds$Productivity),]
 lm_productivity = lme(Productivity ~ Exclosure*NPK, random = ~1|Site/Block, productivity_main)
 anova(lm_productivity)
 round(coef(summary(lm_productivity))[,c(1:2)], 3)
@@ -30,8 +31,7 @@ round(coef(summary(lm_productivity))[,c(1:2)], 3)
 
 ## Offtake
 # 3 outliers removed
-herbivoreofftake_main <- read.table("herbivoreofftake_main.txt", header = T)
-
+herbivoreofftake_main <- biomass_productivity_main_ds[!is.na(biomass_productivity_main_ds$HerbivoreOfftake),]
 lm_herbivoreofftake = lme(HerbivoreOfftake ~ Exclosure*NPK, random = ~1|Site/Block, 
                           subset(herbivoreofftake_main, HerbivoreOfftake < 1000 & HerbivoreOfftake > -500))
 anova(lm_herbivoreofftake)
@@ -42,8 +42,7 @@ round(coef(summary(lm_herbivoreofftake))[,c(1:2)], 3)
 ## Biomass
 # live and dead biomass
 
-biomass_main <- read.table("biomass_main.txt", header = T)
-
+biomass_main <- biomass_productivity_main_ds[!is.na(biomass_productivity_main_ds$Biomass),]
 lm_biomass = lme(Biomass ~ Exclosure*NPK, random = ~1|Site/Block, biomass_main)
 anova(lm_biomass)
 round(coef(summary(lm_biomass))[,c(1:2)], 3)
@@ -53,8 +52,7 @@ round(coef(summary(lm_biomass))[,c(1:2)], 3)
 
 ## Belowground biomass
 
-belowgroundbiomass_main <- read.table("belowgroundbiomass_main.txt", header = T)
-
+belowgroundbiomass_main <- biomass_productivity_main_ds[!is.na(biomass_productivity_main_ds$BelowgroundBiomass),]
 lm_belowgroundbiomass = lme(log(BelowgroundBiomass) ~ Exclosure*NPK, random = ~1|Site/Block, belowgroundbiomass_main)
 anova(lm_belowgroundbiomass)
 
@@ -65,9 +63,7 @@ round(coef(summary(lm_belowgroundbiomass_untransformed))[,c(1:2)], 3)
 
 
 ## Above-belowground productivity ratio
-
-abovebelowground_main <- read.table("abovebelowground_main.txt", header = T)
-
+abovebelowground_main <- biomass_productivity_main_ds[!is.na(biomass_productivity_main_ds$AboveBelowground_Productivity),]
 lm_abovebelowground = lme(log(AboveBelowground_Productivity) ~ Exclosure*NPK, random = ~1|Site/Block, abovebelowground_main)
 anova(lm_abovebelowground)
 round(coef(summary(lm_abovebelowground))[,c(1:2)], 3)
@@ -81,8 +77,7 @@ round(coef(summary(lm_abovebelowground))[,c(1:2)], 3)
 
 ## Biomass predicts productivity
 
-biomass_productivity_main <- read.table("biomass_productivity_main.txt", header = T)
-
+biomass_productivity_main <- biomass_productivity_main_ds[!is.na(biomass_productivity_main_ds$Biomass),]
 lm_productivity_biomass = lme(sqrt(Productivity+2) ~ Biomass*(Exclosure*NPK), random = ~1|Site/Block/Plot, biomass_productivity_main)
 anova(lm_productivity_biomass)
 round(coef(summary(lm_productivity_biomass))[,c(1:2)], 3)
@@ -92,8 +87,6 @@ round(coef(summary(lm_productivity_biomass))[,c(1:2)], 3)
 
 
 ## Productivity with regrowth
-
-regrowth_main <- read.table("regrowth_main.txt", header = T)
 
 lm_regrowth = lme(Productivity ~ Regrowth, random = ~1|Site, regrowth_main)
 anova(lm_regrowth)
